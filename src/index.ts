505af3cad5cb5ef821cd9c850c25d474f84ec6b8
Chris Freeman <cfreeman.code@gmail.com>
2022-01-21 15:50:27 -0700

    more TS-specific stuff

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,72 @@
-import { transformAsync } from '@babel/core';
+import { transformAsync, createConfigItem } from '@babel/core';
+import type { VisitNodeObject, Node } from '@babel/traverse';
+import prettier from 'prettier';
+
+// @ts-expect-error We're only importing so we can create a config item, so we don't care about types
+import bts from '@babel/plugin-transform-typescript';
+
+const babelTsTransform = createConfigItem(bts);
+
+// @ts-expect-error We're only importing so we can create a config item, so we don't care about types
+import bsd from '@babel/plugin-syntax-decorators';
+
+const babelDecoratorSyntax = createConfigItem([bsd, { legacy: true }]);
 
 export default async function removeTypes(code: string) {
-  code = code.replace(/\n\n/g, '/* ___NEWLINE___ */\n');
+  code = code.replace(/\n\n+/g, '/* ___NEWLINE___ */\n');
+  console.log(code);
+
+  // Babel visitor to remove leading comments
+  const removeComments: VisitNodeObject<unknown, Node> = {
+    enter(p) {
+      if (!p.node.leadingComments) return;
+
+      for (let i = p.node.leadingComments.length - 1; i >= 0; i--) {
+        const comment = p.node.leadingComments[i];
+
+        if (
+          code.slice(comment.end).match(/^\s*\n\s*\n/) ||
+          comment.value.includes('___NEWLINE___')
+        ) {
+          // There is at least one empty line between the comment and the TypeScript specific construct
+          // We should keep this comment and those before it
+          break;
+        }
+        comment.value = '___REMOVE_ME___';
+      }
+    },
+  };
 
   const transformed = await transformAsync(code, {
     // retainLines: true,
     plugins: [
-      '@babel/plugin-transform-typescript',
-      [
-        '@babel/plugin-syntax-decorators',
-        {
-          legacy: true,
+      {
+        name: 'comment-remover',
+        visitor: {
+          TSTypeAliasDeclaration: removeComments,
+          TSInterfaceDeclaration: removeComments,
+          TSDeclareFunction: removeComments,
+          TSDeclareMethod: removeComments,
+          TSImportType: removeComments,
         },
-      ],
+      },
+      babelTsTransform,
+      babelDecoratorSyntax,
     ],
+    generatorOpts: {
+      retainLines: true,
+      shouldPrintComment: (comment) => comment !== '___REMOVE_ME___',
+    },
   });
 
-  const fixed = transformed?.code
-    ?.replace(/\/\* ___NEWLINE___ \*\//g, '\n')
-    .replace(/\n\n+/g, '\n');
+  if (!transformed || !transformed.code) {
+    throw new Error('There was an issue with the Babel transform.');
+  }
 
-  return fixed;
+  const fixed = transformed.code.replace(/\/\* ___NEWLINE___ \*\//g, '\n');
+
+  return prettier.format(fixed, {
+    singleQuote: true,
+    parser: 'babel',
+  });
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -8,15 +8,84 @@ function file(filePath: string) {
 }
 
 function fixture(filePath: string) {
-  return file(path.join(process.cwd(), 'test/fixtures', filePath));
+  return file(path.join(process.cwd(), 'test/fixtures', filePath)).trimEnd();
 }
 
 describe('removeTypes', () => {
-  it('works', async () => {
-    const code = await removeTypes(fixture('class.ts'));
-    const expected = fixture('class.js');
-    console.log(code.split('\n').length);
-    console.log(expected.split('\n').length);
-    expect(code).toEqual(expected);
+  it(`preserves default exports`, async () => {
+    const contents = `export default class Foo {}\n`;
+
+    expect(await removeTypes(contents)).toEqual(contents);
+  });
+
+  it('preserves commented function arguments', async () => {
+    const contents = `function foo(params /*, namedParams */) {}\n`;
+
+    expect(await removeTypes(contents)).toEqual(contents);
+  });
+
+  it('leaves decorators alone', async () => {
+    const contents = `class Foo {
+  @blah
+  foo() {}
+}
+`;
+
+    expect(await removeTypes(contents)).toEqual(contents);
+  });
+
+  it('works with numeric enums?', async () => {
+    const contents = `enum LogLevel {
+  ERROR,
+  WARN,
+  INFO,
+  DEBUG,
+}
+
+function f(obj: { X: number }) {
+  return obj.X;
+}
+
+f(E);
+`;
+
+    expect(await removeTypes(contents)).toEqual('foo');
+  });
+
+  it('works with string enums?', async () => {
+    const contents = ``;
+  });
+
+  it('handles all the stuff', async () => {
+    const contents = `import Component from 'component';
+
+// sick interface bro
+interface FooArgs {
+  // this is a number
+  someField: number
+}
+
+// this comment should stay
+export default class Foo extends Component<FooArgs> {
+  // so should this one
+
+  // decorators and class fields whoa
+  @tracked
+  foo = 'foo';
+}
+`;
+
+    const expected = `import Component from 'component';
+
+// this comment should stay
+export default class Foo extends Component {
+  // so should this one
+
+  // decorators and class fields whoa
+  @tracked
+  foo = 'foo';
+}
+`;
+    expect(await removeTypes(contents)).toEqual(expected);
   });
 });
